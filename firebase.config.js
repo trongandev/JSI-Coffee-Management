6981e4b97b15446e5f58b1d97e18f53c09957178
trongandev <trongandev@gmail.com>
2026-05-26 18:51:11 +0700

    first commit

---
+++ firebase.config.js
@@ -0,0 +1,14 @@
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyDZz-nMgoJ6xIpn6wVBrYIyJ1bz_5KWjms",
+    authDomain: "coffe-management-bc84a.firebaseapp.com",
+    projectId: "coffe-management-bc84a",
+    storageBucket: "coffe-management-bc84a.firebasestorage.app",
+    messagingSenderId: "524659671652",
+    appId: "1:524659671652:web:5498aea1d47d3e03069571",
+    measurementId: "G-26B1BLJN37",
+};
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
