2a75579a96fda4b5d618c6c521f3673bb9b0c210
trongandev <trongandev@gmail.com>
2026-06-09 19:15:44 +0700

    fix

--- js/add-product.js
+++ js/add-product.js
@@ -40,4 +40,41 @@ addForm.addEventListener("submit", (e) => {
         validate("image", "Image URL");
         return;
     }
+    console.log({ name, price, description, image });
+    const db = firebase.firestore();
+
+    Swal.fire({
+        title: "Adding Product...",
+        text: "Please wait while we add your product.",
+        allowOutsideClick: false,
+        didOpen: () => {
+            Swal.showLoading();
+        },
+    });
+    // Add a new document with a generated id.
+    db.collection("products")
+        .add({
+            name: name,
+            price: price,
+            description: description,
+            image: image,
+        })
+        .then((docRef) => {
+            console.log("Document written with ID: ", docRef.id);
+            Swal.fire({
+                icon: "success",
+                title: "Product Added",
+                text: "Your product has been added successfully.",
+            }).then(() => {
+                addForm.reset();
+            });
+        })
+        .catch((error) => {
+            console.error("Error adding document: ", error);
+            Swal.fire({
+                icon: "error",
+                title: "Error",
+                text: "There was an error adding your product. Please try again.",
+            });
+        });
 });
--- main.js
+++ main.js
@@ -1,1 +1,2 @@
-console.log(firebase.app().name);
+
+
